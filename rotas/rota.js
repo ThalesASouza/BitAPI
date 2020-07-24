c69d396eff69018ae9ec93f98e001a4a6ad094d7
ThalesASouza <thalessouza16@yahoo.com.br>
2020-07-24 13:02:01 -0300

    Arquivo rota.js modificado

--- rotas/rota.js
+++ rotas/rota.js
@@ -1,26 +1,27 @@
-import express from 'express';
-import {bitixModel } from '../model/modelBitix.js';
+import express from "express";
+import { bitixModel } from "../model/modelBitix.js";
 
 const rota = express.Router();
 
-rota.get('/periodoBitix', async (req,res)=>{
-    const {inicial,final} = req.query;
-    const periodoInicial = new Date(inicial);
-    const periodoFinal = new Date(final);
-    
-    try{
-        const consulta = await bitixModel.find(
-            {$and:[{datacriacao:{$gte:periodoInicial}},{datacriacao:{$lte:periodoFinal}}]}
-        )
-        if (!consulta) {
-            res.status(400).send("Periodo não encontrado");
-        }
-        res.send(consulta);
-    }catch(err){
-        res.status(500).send(err);
+rota.get("/periodoBitix", async (req, res) => {
+  const { inicial, final } = req.query;
+  const periodoInicial = new Date(inicial);
+  const periodoFinal = new Date(final);
+
+  try {
+    const consulta = await bitixModel.find({
+      $and: [
+        { datacriacao: { $gte: periodoInicial } },
+        { datacriacao: { $lte: periodoFinal } },
+      ],
+    });
+    if (consulta.length === 0) {
+      res.status(400).send({ err: "Periodo não encontrado" });
     }
-})
+    res.send(consulta);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 export default rota;
-
-
